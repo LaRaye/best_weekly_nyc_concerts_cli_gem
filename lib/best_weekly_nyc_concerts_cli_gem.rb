5156be69c3ed0c78332ce1c74e60b6df87e9abbe
LaRaye <lnj2105@gmail.com>
2019-03-10 18:07:48 -0400

    initial set-up

---
+++ lib/best_weekly_nyc_concerts_cli_gem.rb
@@ -0,0 +1,6 @@
+require "best_weekly_nyc_concerts_cli_gem/version"
+
+module BestWeeklyNycConcertsCliGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_weekly_nyc_concerts_cli_gem/version.rb
@@ -0,0 +1,3 @@
+module BestWeeklyNycConcertsCliGem
+  VERSION = "0.1.0"
+end
