77fca44c6f8a735de043d490799f14c6237e0d02
LaRaye <lnj2105@gmail.com>
2019-03-13 17:37:33 -0400

    begin scraping for concerts

--- lib/best_weekly_nyc_concerts/concert_scraper.rb
+++ lib/best_weekly_nyc_concerts/concert_scraper.rb
@@ -3,5 +3,11 @@ class BestWeeklyNycConcerts::Concert_Scraper
   def get_page
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/music/the-best-concerts-in-nyc-this-week"))
     binding.pry
+
+    #title = doc.css(".card-content").first.css("h3").text.strip
+    #location = doc.css(".card-content").first.css("span.bold").text.strip
+    #genre = doc.css(".card-content").first.css(".category").text.strip
+    #date = doc.css(".card-content").first.css("time.bold").text.strip
+    #blurb = doc.css(".card-content").first.css(".js-card-desc-content").text.strip
   end
 end
