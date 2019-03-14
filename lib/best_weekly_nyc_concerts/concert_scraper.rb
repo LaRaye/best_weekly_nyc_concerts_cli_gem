731e380995a031a870996e5d7ebf3ffdb090b7b1
LaRaye <lnj2105@gmail.com>
2019-03-13 21:21:42 -0400

    refactor scraper methods

--- lib/best_weekly_nyc_concerts/concert_scraper.rb
+++ lib/best_weekly_nyc_concerts/concert_scraper.rb
@@ -2,15 +2,20 @@ class BestWeeklyNycConcerts::Concert_Scraper
 
   def get_page
     @doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/music/the-best-concerts-in-nyc-this-week"))
-
-    @doc.css(".card-content").each do |concert_listing|
-     concert = BestWeeklyNycConcerts::Concert.new
-     concert.title = concert_listing.css("h3").text.strip
-     concert.location = concert_listing.css("span.bold").text.strip
-     concert.date = concert_listing.css("time.bold").text.strip
-     concert.genre = concert_listing.css(".category").text.strip
-     concert.blurb = concert_listing.css(".js-card-desc-content").text.strip
-    end
   end
 
+  def get_concerts
+    self.get_page.css(".card-content")
+  end
+
+  def create_concerts
+    self.get_concerts.each do |concert_listing|
+      concert = BestWeeklyNycConcerts::Concert.new
+      concert.title = concert_listing.css("h3").text.strip
+      concert.location = concert_listing.css("span.bold").text.strip
+      concert.date = concert_listing.css("time.bold").text.strip
+      concert.genre = concert_listing.css(".category").text.strip
+      concert.blurb = concert_listing.css(".js-card-desc-content").text.strip
+    end
+  end
 end
